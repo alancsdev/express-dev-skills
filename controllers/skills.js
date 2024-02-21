147723ece9cd521cdf2b222d10f58d688efe3d9d
Alan Santos <alanchs@live.com>
2024-02-21 18:01:52 +0800

    finishing express

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,16 +3,53 @@ const Skill = require('../models/skill');
 module.exports = {
   index,
   show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update,
 };
+
+function update(req, res) {
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', {
+    title: 'Edit Skill',
+    skill,
+  });
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+
+function create(req, res) {
+  console.log(req.body);
+  // Models are responible for CRUD'ing the data
+  Skill.create(req.body);
+  // Always do a redirect when data has been changed
+  res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'New Skill' });
+}
 
 function index(req, res) {
   res.render('skills/index', {
     skills: Skill.getAll(),
+    title: 'All Skills',
   });
 }
 
 function show(req, res) {
   res.render('skills/show', {
     skill: Skill.getOne(req.params.id),
+    title: 'Skill Details',
   });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -3,8 +3,21 @@ var router = express.Router();
 
 var skillsController = require('../controllers/skills');
 
-router.get('/', skillsController.index);
+// All actual paths start with "/skills"
 
+// GET /skills
+router.get('/', skillsController.index);
+// GET /skills/new <- Define BEFORE show route
+router.get('/new', skillsController.new);
+// GET /skills/:id
 router.get('/:id', skillsController.show);
+// GET /skills/:id/edit
+router.get('/:id/edit', skillsController.edit);
+// POST /skills
+router.post('/', skillsController.create);
+// DELETE /skills/:id
+router.delete('/:id', skillsController.delete);
+// PUT /skills/:id
+router.put('/:id', skillsController.update);
 
 module.exports = router;
